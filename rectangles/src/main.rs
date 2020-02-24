9a119fd8ab9712d8e957641c170e0d4a9db82934
NomadicDaggy <dagsagrinbergs@gmail.com>
2020-02-24 23:02:30 +0200

    Derive debug to print instance of Rectangle

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]  // this is an annotation
 struct Rectangle {
     width: u32,
     height: u32,
@@ -6,12 +7,18 @@ struct Rectangle {
 fn main() {
     let rect1 = Rectangle { width: 30, height : 50 };
 
+    // Print Rectangle instance, by deriving Debug trait in line 1
+    // Can also pretty print by using {:#?} instead of {:?}
+    println!("rect1 is {:?}", rect1);
+
     println!(
         "The area of the rectangle is {} square pixels.",
         area(&rect1)
     );
 }
 
+// Take a reference to Rectangle, so that main can keep using it
+// i.e. so that we don't have to explicitly pass it back
 fn area(rectangle: &Rectangle) -> u32 {
     rectangle.width * rectangle.height
 }
