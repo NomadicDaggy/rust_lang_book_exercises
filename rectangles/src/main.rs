8117e7a1f932e89a78b73a7be73888992a059778
NomadicDaggy <dagsagrinbergs@gmail.com>
2020-02-25 20:17:57 +0200

    Explanations

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,11 +1,12 @@
-#[derive(Debug)]  // this is an annotation
+#[derive(Debug)]  // This is called an annotation
 struct Rectangle {
     width: u32,
     height: u32,
 }
-
+// To define a struct method, you have to put it inside a impl block.
+// It's main purpose is to organize all struct methods in one place.
 impl Rectangle {
-    fn area(&self) -> u32 {
+    fn area(&self) -> u32 {  // and functions have to have (&)(mut) self as 1st arg
         self.width * self.height
     }
 }
