1fcb404a2ea34a2c1e53994f92c53dd4098a7edc
NomadicDaggy <dagsagrinbergs@gmail.com>
2020-02-26 18:31:26 +0200

    abs and relat path

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,5 +1,7 @@
-mod front_of_house {
-    mod hosting {
+mod front_of_house {  // This is a module
+    mod hosting {  // Modules can be nested
+        // And modules can have definitions of a bunch of things
+        // like functions
         fn add_to_waitlist() {}
 
         fn seat_at_table() {}
@@ -12,4 +14,12 @@ mod front_of_house {
 
         fn take_payment() {}
     }
+}
+
+pub fn eat_at_restaurant() {
+    // Absolute Path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative Path
+    front_of_house::hosting::add_to_waitlist();
 }
