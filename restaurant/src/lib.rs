9adeb53e30c113fb27a401293b71acd4faffd62d
NomadicDaggy <dagsagrinbergs@gmail.com>
2020-02-26 18:38:26 +0200

    add public

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,8 +1,9 @@
 mod front_of_house {  // This is a module
-    mod hosting {  // Modules can be nested
+    // Making the module public, does not make its contents public
+    pub mod hosting {  // Modules can be nested
         // And modules can have definitions of a bunch of things
         // like functions
-        fn add_to_waitlist() {}
+        pub fn add_to_waitlist() {}
 
         fn seat_at_table() {}
     }
