675d7249b18e63bb697b40ba962d19dbbb3f9ec4
NomadicDaggy <dagsagrinbergs@gmail.com>
2020-02-26 18:48:07 +0200

    Public enum

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -17,21 +17,42 @@ mod front_of_house {  // This is a module
     }
 }
 
-fn serve_order() {}
-
 mod back_of_house {
-    fn fix_incorrect_order() {
-        cook_order();
-        super::serve_order(); // super is like ".."- you climb a level up
+    // Making a struct public, doesn't make its fields public
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
     }
 
-    fn cook_order() {}
+    // This is how you define struct methods
+    impl Breakfast {
+        // This has to be public, otherwise we can't construct the struct
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    // If you make an enum public, all its variants become public too
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
 }
 
 pub fn eat_at_restaurant() {
-    // Absolute Path
-    crate::front_of_house::hosting::add_to_waitlist();
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about what bread we'd like
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
 
-    // Relative Path
-    front_of_house::hosting::add_to_waitlist();
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
+
+    // The next line won't compile if we uncomment it; we're not allowed
+    // to see or modify the seasonal fruit that comes with the meal
+    // meal.seasonal_fruit = String::from("blueberries");
 }
