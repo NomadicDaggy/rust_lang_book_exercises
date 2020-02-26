52ebc4a39db4c6a862a8d75932ddcc7032efff95
NomadicDaggy <dagsagrinbergs@gmail.com>
2020-02-26 19:00:09 +0200

    pub use

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -42,6 +42,10 @@ mod back_of_house {
     }
 }
 
+// By putting pub here, hosting becomes globally public
+// It's called re-exporting
+pub use crate::front_of_house::hosting;
+
 pub fn eat_at_restaurant() {
     // Order a breakfast in the summer with Rye toast
     let mut meal = back_of_house::Breakfast::summer("Rye");
@@ -51,6 +55,8 @@ pub fn eat_at_restaurant() {
 
     let order1 = back_of_house::Appetizer::Soup;
     let order2 = back_of_house::Appetizer::Salad;
+
+    hosting::add_to_waitlist();
 
     // The next line won't compile if we uncomment it; we're not allowed
     // to see or modify the seasonal fruit that comes with the meal
