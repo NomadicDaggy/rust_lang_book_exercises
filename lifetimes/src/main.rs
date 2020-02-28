1e18d618842bc282225c0b4ea4c1f1c57278d8a0
NomadicDaggy <dagsagrinbergs@gmail.com>
2020-02-28 22:58:09 +0200

    Annotations and another example

--- lifetimes/src/main.rs
+++ lifetimes/src/main.rs
@@ -1,3 +1,6 @@
+// Setting lifetimes doesn't change how long a value lives
+// it just makes clear for the compiler how long each value
+// should live, so it can reject the ones that misbehave.
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {
         x
@@ -6,10 +9,13 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     }
 }
 
+// Working example
 fn main() {
-    let string1 = String::from("abcd");
-    let string2 = "xyz";
+    let string1 = String::from("long string is long");
 
-    let result = longest(string1.as_str(), string2);
-    println!("The longest string is {}", result);
+    {
+        let string2 = String::from("xyz");
+        let result = longest(string1.as_str(), string2.as_str());
+        println!("The longest string is {}", result);
+    }
 }
