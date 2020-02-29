bd09b61dd8120e844d0007896c20e8550b28306f
NomadicDaggy <dagsagrinbergs@gmail.com>
2020-02-29 18:19:15 +0200

    Add tests submodule for setting up

---
+++ adder/tests/common/mod.rs
@@ -0,0 +1,5 @@
+// Files in subdirectories of the tests directory don't get compiled
+// as separate crates or have sections in the test output.
+pub fn setup() {
+    // setup code specific to your library's tests would go here
+}
