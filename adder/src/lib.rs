5f257e034621096b71ba74672371aefbbb2deecb
NomadicDaggy <dagsagrinbergs@gmail.com>
2020-02-29 16:17:56 +0200

    Add Guests test module

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -71,3 +71,29 @@ mod tests {
         );
     }
 }
+
+// ----------------------------------------
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess { value }
+    }
+}
+
+#[cfg(test)]
+mod more_tests {  // More than 1 test modules can exist in one file :)
+    use super::*;
+
+    #[test]
+    #[should_panic]  // test passes if we panic in the next block
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+}
