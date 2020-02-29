c6237c5de39c93af7f7c897ccb776907e4ddf389
NomadicDaggy <dagsagrinbergs@gmail.com>
2020-02-29 16:22:26 +0200

    should_panic with specific panic output

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -79,8 +79,13 @@ pub struct Guess {
 
 impl Guess {
     pub fn new(value: i32) -> Guess {
-        if value < 1 || value > 100 {
+        if value < 1 {
             panic!("Guess value must be between 1 and 100, got {}.", value);
+        } else if value > 100 {
+            panic!(
+                "Guess value must be less than or equal to 100, got {}.",
+                value
+            );
         }
 
         Guess { value }
@@ -88,11 +93,13 @@ impl Guess {
 }
 
 #[cfg(test)]
-mod more_tests {  // More than 1 test modules can exist in one file :)
+mod more_tests {
+    // More than 1 test modules can exist in one file :)
     use super::*;
 
     #[test]
-    #[should_panic]  // test passes if we panic in the next block
+    //#[should_panic] // test passes if we panic in the next block
+    #[should_panic(expected = "Guess value must be less than or equal to 100")]
     fn greater_than_100() {
         Guess::new(200);
     }
